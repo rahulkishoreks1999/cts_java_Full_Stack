b7961e4c057c163f18aa2d53c46ba5692a740d3e
dev@gmail.com <you@example.com>
2021-06-18 10:27:14 +0530

    Dev has to modifiy test.java

--- firstproject/src/com/cts/beans/Test.java
+++ firstproject/src/com/cts/beans/Test.java
@@ -1,10 +1,10 @@
 package com.cts.beans;
 
-public class Test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+import java.util.Date;
+public class Test
+{
+	public Date getSystemDate()
+	{
+		return new Date();
 	}
-
 }
